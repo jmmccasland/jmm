417ddfae042adcee473a91dc002ca058d2219030
John Michael McCasland <jmmccasland@gmail.com>
2017-05-09 12:18:47 -0500

    Sass Cleanup

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
 import 'whatwg-fetch';
+
+const arrow = document.querySelector('.hero__arrow');
+const element = document.querySelector('.section--work');
+
+arrow.addEventListener('click', () => {
+  console.log('YES');
+  // element.scrollIntoView();
+  // element.scrollIntoView(false);
+  // element.scrollIntoView({ block: 'end' });
+  // element.scrollIntoView({ block: 'end', behavior: 'smooth' });
+  window.scrollTo(0, 300);
+});

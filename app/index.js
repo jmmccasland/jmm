f6d2009d66d81c8ba17959ddeda584a23df84057
John Michael McCasland <jmmccasland@gmail.com>
2017-05-10 12:00:58 -0500

    Detail layout refactor

--- app/index.js
+++ app/index.js
@@ -5,5 +5,9 @@ import jump from 'jump.js';
 const element = document.querySelector('.hero__arrow');
 
 element.addEventListener('click', () => {
-  jump('#work');
+  jump('.target', {
+    offset: -60,
+    callback: () => console.log('Hey! Thanks for caring enough to check this out! I used jump.js for that fancy scroll you just saw. Thanks, Michael! You can check his work out here: https://github.com/callmecavs/jump.js')
+
+  });
 });

635a5498de473622ec1df260790cc0b2fafa6de0
John Michael McCasland <jmmccasland@gmail.com>
2017-05-10 21:02:58 -0500

    iOS vh hack #2

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const element = document.querySelector('.hero__arrow');
 
 element.addEventListener('click', () => {
   jump('.target', {
-    offset: -60,
+    offset: -59,
     callback: () => console.log('Hey! Thanks for caring enough to check this out! I used jump.js for that fancy scroll you just saw. Thanks, Michael! You can check his work out here: https://github.com/callmecavs/jump.js')
 
   });

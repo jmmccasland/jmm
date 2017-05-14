b6ea9feb980b74443236cec2383f9c9bc9fc5f85
John Michael McCasland <jmmccasland@gmail.com>
2017-05-13 22:06:09 -0500

    Wrapper transitions

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import 'whatwg-fetch';
-import jump from 'jump.js';
+// import jump from 'jump.js';
 
 // Is the menu open?
 let menuOpen = false;
@@ -8,26 +8,33 @@ let menuOpen = false;
 const menuBtn = document.querySelector('.menu-btn');
 
 // Menu Variables
-const body = document.querySelector('body');
+const body = document.querySelector('.body');
 const menu = document.querySelector('.section--menu');
+const wrapper = document.querySelector('.wrapper');
+const topNav = document.querySelector('.section--top-nav');
+
 
 // Hamburger Variables
 const mid = document.querySelector('.line--mid');
 const topLine = document.querySelector('.line--top');
 const bottom = document.querySelector('.line--bottom');
 
-
+// Menu Toggle Function
 function toggleMenu() {
   if (!menuOpen) {
-    menu.classList.add('section--menu--open');
     body.classList.add('body--open');
+    menu.classList.add('section--menu--open');
+    wrapper.classList.add('wrapper--open');
+    topNav.classList.add('section--top-nav--open');
     mid.classList.add('line--mid--open');
     topLine.classList.add('line--top--open');
     bottom.classList.add('line--bottom--open');
     menuOpen = !menuOpen;
   } else {
-    menu.classList.remove('section--menu--open');
     body.classList.remove('body--open');
+    menu.classList.remove('section--menu--open');
+    wrapper.classList.remove('wrapper--open');
+    topNav.classList.remove('section--top-nav--open');
     mid.classList.remove('line--mid--open');
     topLine.classList.remove('line--top--open');
     bottom.classList.remove('line--bottom--open');
@@ -35,7 +42,7 @@ function toggleMenu() {
   }
 }
 
-// Menu Btn - Menu Toggle
+// Call the Toggle Function on the Menu Btn Click
 menuBtn.addEventListener('click', () => {
   toggleMenu();
 });
